b4393fa7cec2e0406215f8ad93eb9a03192fbf33
jjjfay <juliacfay3@gmail.com>
2024-05-21 13:51:15 -0700

    testing doxygen

--- jjjfay.github.io/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['4000000_0',['4000000',['../system__stm32l4xx_8c.html#autotoc_md3',1,'HCLK(Hz)                               | 4000000'],['../system__stm32l4xx_8c.html#autotoc_md2',1,'SYSCLK(Hz)                             | 4000000']]]
-];
